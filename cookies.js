fa9d4d0320c9b505f0de5376f5f8665922efccf6
Akihiko <julien.salesse@icloud.com>
2026-01-31 12:05:06 +0100

    Refactor initCookies function and style setup

--- cookies.js
+++ cookies.js
@@ -222,7 +222,7 @@ function initCookies() {
     };
 
 // Configuration Google Analytics 4
-    tarteaucitron.user.gtagUa = 'G-XXXXXXXXXX'; // Remplace par ton ID
+    tarteaucitron.user.gtagUa = 'G-SYGFFHLSDC'; // Remplace par ton ID
     
     // Ajout de la configuration pour l'anonymisation
     tarteaucitron.user.gtagMore = function () {
@@ -233,5 +233,6 @@ function initCookies() {
     (tarteaucitron.job = tarteaucitron.job || []).push('custommaps');
     (tarteaucitron.job = tarteaucitron.job || []).push('spline');
 }
+
 
 initCookies();
